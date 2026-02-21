50b82a3cee753e69ea539d6a3db84caba579afc1
suis <suissetiawan11@gmail.com>
2026-02-21 11:53:39 +0700

    feat: Initialize Spring Boot project with Maven wrapper and basic application structure.

---
+++ src/main/java/com/dibimbing/medicareflow/MedicareflowApplication.java
@@ -0,0 +1,13 @@
+package com.dibimbing.medicareflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicareflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicareflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dibimbing/medicareflow/MedicareflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dibimbing.medicareflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicareflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
